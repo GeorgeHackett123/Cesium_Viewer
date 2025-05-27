5036e50c2944193c9c95adbcdf36ab82e7b10be6
GeorgeHackett123 <george.hackett.gis@gmail.com>
2025-05-27 10:40:47 -0300

    bat edit

--- generate_manifest.bat
+++ generate_manifest.bat
@@ -5,23 +5,21 @@ set "data_dir=data"
 set "output_file=%data_dir%\manifest_other.json"
 set "exclude=camera_points_final.geojson"
 
-(
-    echo [
-    set "first=1"
-    for %%f in (%data_dir%\*.geojson) do (
-        set "filename=%%~nxf"
-        if /i not "!filename!"=="%exclude%" (
-            if !first! == 1 (
-                set "first=0"
-            ) else (
-                echo,
-            )
-            <nul set /p="    "!filename!""
-        )
-    )
-    echo
-    echo ]
-) > "%output_file%"
+> "%output_file%" echo [
 
-echo ✅ manifest_other.json successfully created at: %output_file%
+set "first=1"
+for %%f in (%data_dir%\*.geojson) do (
+    set "filename=%%~nxf"
+    if /i not "!filename!"=="%exclude%" (
+        if !first! == 1 (
+            >> "%output_file%" echo,
+        )
+        >> "%output_file%" echo   "!filename!"
+        set first=0
+    )
+)
+
+>> "%output_file%" echo ]
+
+echo ✅ Pretty JSON manifest written to %output_file%
 pause
